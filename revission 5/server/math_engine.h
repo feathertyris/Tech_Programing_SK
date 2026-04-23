71d15f4a5b9f8f3bd186dd3fb131819dfbe3cde9
feathertyris <sudnikvlad8@gmail.com>
2026-04-23 23:19:23 +0300

    Add files via upload

---
+++ revission 5/server/math_engine.h
@@ -0,0 +1,32 @@
+#ifndef MATHENGINE_H
+#define MATHENGINE_H
+
+#include <QVector>
+#include <limits>
+
+struct Point {
+    double x;
+    double y;
+};
+
+struct SystemFunctionParams {
+    double a, b, c;
+    double xStart, xEnd, step;
+
+    SystemFunctionParams(double a_, double b_, double c_,
+                         double xStart_, double xEnd_, double step_)
+        : a(a_), b(b_), c(c_),
+        xStart(xStart_), xEnd(xEnd_), step(step_) {}
+};
+
+class MathEngine
+{
+public:
+    static QVector<Point> generateSystemPoints(double a, double b, double c,
+                                               double xStart, double xEnd, double step);
+
+private:
+    static double calculateFunction(double x, double a, double b, double c);
+};
+
+#endif // MATHENGINE_H
