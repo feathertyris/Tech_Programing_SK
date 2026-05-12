e8501f317d0a93d5b979b3c6e0cd7e4214d8080b
feathertyris <sudnikvlad8@gmail.com>
2026-05-12 21:52:43 +0300

    Update main.cpp

--- revission_09_python_version/server/main.cpp
+++ revission_09_python_version/server/main.cpp
@@ -1,10 +1,9 @@
-#include <QCoreApplication>
+#include <QCoreApplication> //  базовый класс Qt для консольных приложений (без графического интерфейса)
 #include "postgresqlserver.h"
 
 int main(int argc, char *argv[]) {
-    QCoreApplication app(argc, argv);
+    QCoreApplication app(argc, argv); // Создаётся объект app типа QCoreApplication
 
-    PostgreSQLServer server;
-
-    return app.exec();
+    PostgreSQLServer server; // Создание объекта сервера
+    return app.exec(); // запуск обработки событий . Завершится только когда кто-то вызовет app.quit()
 }
